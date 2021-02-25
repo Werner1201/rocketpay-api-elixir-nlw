9733ec70e46e79fec194007822a93880f72c2ff2
Werner1201 <werner.romling@unigranrio.br>
2021-02-25 09:13:43 -0300

    Terminando a rota de deposit e withdraw

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -6,4 +6,5 @@ defmodule Rocketpay do
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
