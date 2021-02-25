44ed37fe6b3041cf62c61bd9dca836752d5c9caa
Werner1201 <werner.romling@unigranrio.br>
2021-02-25 10:41:16 -0300

    Struct de Resposta de Transacao para ficar mais claro de ler

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,5 +1,7 @@
 defmodule RocketpayWeb.AccountsView do
   alias Rocketpay.Account
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
+
 
   def render("update.json", %{account: %Account{id: account_id, balance: balance}}) do
     %{
@@ -9,5 +11,21 @@ defmodule RocketpayWeb.AccountsView do
           balance: balance
         }
       }
+  end
+
+  def render("transaction.json", %{transaction: %TransactionResponse{to_account: to_account, from_account: from_account}}) do
+    %{
+      message: "Transaction done successfully",
+        transaction:  %{
+          from_account: %{
+          id: from_account.id,
+          balance: from_account.balance
+        },
+        to_account: %{
+          id: to_account.id,
+          balance: to_account.balance
+        }
+      }
+    }
   end
 end
